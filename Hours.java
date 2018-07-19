206f2d9bae6ee1945e6a58b55e20501a115d4c45
techdem <tudor.chiribes@gmail.com>
2018-07-19 08:40:37 +0100

    removed ide headings

--- Hours.java
+++ Hours.java
@@ -1,21 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package hours;
 import java.util.*;
 import java.time.*;
 
-/**
- *
- * @author chiribest
- */
 public class Hours {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner scan = new Scanner(System.in);
